ba0af5427cc933777a1ab9e30f5a4b809b9188c1
Chiicken69 <adamcola06@gmail.com>
2025-05-03 23:19:40 +0200

    Updated ball script for text rendering

--- EXAMPROJECT/Assets/Scripts/Monobehaviour/Ball.cs
+++ EXAMPROJECT/Assets/Scripts/Monobehaviour/Ball.cs
@@ -1,13 +1,20 @@
 using System;
+using TMPro;
 using UnityEngine;
 using Color = System.Drawing.Color;
 
 public class Ball : MonoBehaviour, ISaveable, ILoadable
 {
     
+    
     // Isaveable implementation
     public string id;
     private  Vector2 position;
+    
+    public SaveItem SaveData()
+    {
+        return new SaveItem(){id = this.id, position = this.transform.position};
+    }
 
     
     //Iloadable implementation
@@ -15,7 +22,6 @@ public class Ball : MonoBehaviour, ISaveable, ILoadable
     {
         return id;
     }
-
     public void LoadData(SaveItem data)
     {
         transform.position = new Vector2(data.position.x, data.position.y);
@@ -23,14 +29,44 @@ public class Ball : MonoBehaviour, ISaveable, ILoadable
 
 
     //ball stuff
+    public TextMeshProUGUI text;
+    public Transform TempTrans;
     
-    
-
-
-
-
-    public SaveItem SaveData()
+    private void Awake()
     {
-        return new SaveItem(){id = this.id, position = this.transform.position};
+        FindReferences();
     }
+
+    private void Update()
+    {
+        UpdateText();
+    }
+
+   
+    private void FindReferences()
+    {
+        //bit of a weird way to get a component ina grandchild.. but it works
+        TempTrans = this.GetComponentInChildren<Transform>();
+        text = TempTrans.GetComponentInChildren<TextMeshProUGUI>();
+        if (TempTrans == null)
+        {
+            Debug.LogWarning("Trans is null");
+        }
+        if (text == null)
+        {
+            Debug.LogWarning("Text is null");
+        }
+    }
+
+    private void UpdateText()
+    {
+        text.text = "x: " + transform.position.x + ", y: " + transform.position.y;
+    }
+    
+    
+
+
+
+
+   
 }
