163394854eb35571cd6d8c64d8674475f584461d
Chiicken69 <adamcola06@gmail.com>
2025-05-03 17:49:16 +0200

    Created some scripts and a ball

---
+++ EXAMPROJECT/Assets/Scripts/Misc_/SaveItem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class SaveItem
+{
+    public string id;
+    public string type;
+    public float x, y, z;
+}
---
+++ EXAMPROJECT/Assets/Scripts/Misc_/SaveWrapper.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveWrapper : MonoBehaviour
+{
+    public List<SaveItem> items = new List<SaveItem>();
+}
